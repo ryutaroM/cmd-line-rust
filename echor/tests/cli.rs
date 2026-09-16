842beefa9550ec2f11c150e8f4d9a284e913666e
ryutaroM <arcticmonkey326@gmail.com>
2026-09-17 00:59:54 +0900

    tests: add helper and some tests

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,16 +1,54 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.assert()
+fn dies_no_args() -> TestResult {
+    Command::cargo_bin("echor")?
+        .assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
-fn runs(){
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("Hello").assert().success();
+fn runs() -> TestResult {
+    Command::cargo_bin("echor")?.arg("Hello").assert().success();
+    Ok(())
+}
+
+#[test]
+fn hello1() -> TestResult {
+    run(&vec!["Hello there"], "tests/expected/hello1.txt")?;
+    Ok(())
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&vec!["Hello", "there"], "tests/expected/hello2.txt")?;
+    Ok(())
+}
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&vec!["Hello  there", "-n"], "tests/expected/hello1.n.txt")?;
+    Ok(())
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&vec!["-n", "Hello", "there"], "tests/expected/hello2.n.txt")?;
+    Ok(())
 }
