a8549fbe65e94a3b4626ca4af31c9fa731d8d7ac
ryutaroM <arcticmonkey326@gmail.com>
2026-09-26 23:25:35 +0900

    feat: parse args and options

---
+++ wcr/src/lib.rs
@@ -0,0 +1,76 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: bool,
+    words: bool,
+    bytes: bool,
+    chars: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("wcr")
+        .version("0.1.0")
+        .author("ryutaroM")
+        .about("Rust wc")
+        .arg(
+            Arg::with_name("lines")
+                .short("l")
+                .long("lines")
+                .help("Show line count")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("words")
+                .short("w")
+                .long("words")
+                .help("Show word count")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .help("Show byte count")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("chars")
+                .short("m")
+                .long("chars")
+                .help("Show character count")
+                .takes_value(false)
+                .conflicts_with("bytes"),
+        )
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILES")
+                .help("Input file(s)")
+                .default_value("-")
+                .multiple(true),
+        )
+        .get_matches();
+
+    let lines = matches.is_present("lines");
+    let words = matches.is_present("words");
+    let bytes = matches.is_present("bytes");
+    let chars = matches.is_present("chars");
+    let files = matches.values_of_lossy("files").unwrap();
+
+    Ok(Config {
+        files,
+        lines,
+        words,
+        bytes,
+        chars,
+    })
+}
+
+pub fn run(cfg: Config) -> MyResult<()> {
+    println!("{:#?}", cfg);
+    Ok(())
+}
